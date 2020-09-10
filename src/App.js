74e578af224b3d1b585fbdfaf194507510dd5994
kchilds2020 <63356648+kchilds2020@users.noreply.github.com>
2020-09-09 20:28:28 -0500

    remove left navigation if on mobile

--- src/App.js
+++ src/App.js
@@ -1,35 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-
-  const header = {
-    fontSize: '32px',
-    fontWeight: 'bold',
-    padding: '20px',
-    textAlign: 'center'
-  }
-
-  const left = {
-    backgroundColor: '#f4f4f4',
-    position: 'fixed',
-    width: '200px',
-    height: '100vh',
-    left: '0',
-    top: '0'
-  }
-
-  const right = {
-    marginLeft: '200px',
-    padding: '20px',
-    fontSize: '20px'
-  }
+  const[active, setActive] = useState('');
 
   return (
     <>
-      <div style={header}>Sorting Alogorithms</div>
-      <div style={left} ></div>
-      <div style={right} >
+      <div className = "left" >
+        <ul>
+          <li><a href="#bubblesort" className={ active === 'bubblesort' ? 'active' : '' } onClick ={() => setActive('bubblesort')}>Bubble Sort</a></li>
+          <li><a href="#selectionsort" className={ active === 'selectionsort' ? 'active' : '' } onClick ={() => setActive('selectionsort')}>Selection Sort</a></li>
+          <li><a href="#mergesort" className={ active === 'mergesort' ? 'active' : '' } onClick ={() => setActive('mergesort')}>Merge Sort</a></li>
+        </ul>
+      </div>
+      <div className="right" >
+      <div className="header">Sorting Alogorithms</div>
         <p>This page will demonstrate the various different kinds of sorting algorithms and their time complexities. There will also be coding examples in C for each of the coding algorithms. </p>
         <br/>
         <div id="bubblesort">
@@ -40,11 +25,7 @@ function App() {
 #include<stdio.h>
 #include<print_array.h>
 
-int main(){
-
-  int array[10] = {8,5,4,3,2,6,5,4,3,2};
-  int arrayCount = 10;
-
+void bubbleSort(array, arrayCount){
   for(int i = 0; i < arrayCount - 1){
     for(int j = 0; j < arrayCount - i - j; j++){
       //if the next number is larger than the previous number in the array, swap
@@ -58,6 +39,14 @@ int main(){
       }
     }
   }
+}
+
+int main(){
+  int array[10] = {8,5,4,3,2,6,5,4,3,2};
+  int arrayCount = 10;
+
+  bubbleSort(array, arrayCount);
+
   return 0;
 }
           `}
@@ -107,24 +96,64 @@ int main(){
 #include<stdio.h>
 #include<print_array.h>
 
-int main(){
+void merge(){
+  int i, j, k;
+  int n1 = m-l + 1;
+  int n2 = r - m;
+  int L[n1], R[n2];
 
+  for(i = 0; i < n1; i++)
+    L[i] = arr[l+i];
+
+  for(j = 0; j < n2; j++)
+    R[j] = arr[m + 1 + j];
+
+  j = 0;
+  i = 0;
+  k = l;
+
+  while(i < n1 && j < n2){
+    if(L[i] <= R[j]){
+      arr[k] = L[i];
+      i++;
+    } else{
+      arr[k] = R[j];
+      j++;
+    }
+    k++;
+  }
+
+  while(i < n1){
+    arr[k] = L[i];
+    i++;
+    k++;
+  }
+
+  while(j < n2){
+    arr[k] = R[j];
+    j++;
+    k++;
+  }
+
+}
+
+void mergeSort(int arr[], int l, int r){
+  if(l < r){
+    let m = l + (r - l) / 2;
+    int len = r + 1 - l;
+    mergeSort(arr, l, m);
+    mergeSort(arr, m+1, r);
+
+    merge(arr, l, m ,r);
+  }
+}
+
+int main(){
   int array[10] = {8,5,4,3,2,6,5,4,3,2};
   int arrayCount = 10;
 
-  for(int i = 0; i < arrayCount - 1){
-    for(int j = 0; j < arrayCount - i - j; j++){
-      //if the next number is larger than the previous number in the array, swap
-      if(array[j] > array[j+1]){
+  mergeSort(array, 0, arrayCount - 1);
 
-        int temp = array[j];
-        array[j] = array[j+1];
-        array[j+1] = temp;
-
-        printArr(array, arrayCount);
-      }
-    }
-  }
   return 0;
 }
           `}
